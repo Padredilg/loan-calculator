2de49657f650027eed9872205842a731152bc16a
Padredilg <padredilg@knights.ucf.edu>
2023-02-11 17:35:05 -0500

    committing cpp file base main

---
+++ LuizPadredi_Assignment2.cpp
@@ -0,0 +1,16 @@
+/*
+Author: Luiz Padredi
+Date: 02/11/2023
+Assignment 2: Loan Calculator
+Repository: https://github.com/Padredilg/loan-calculator
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Loan Calculator";
+    return 0;
+}
