3a0ae2df973ba3307146886509a6cdee620252c1
Padredilg <padredilg@knights.ucf.edu>
2023-02-13 22:35:06 -0500

    created function to display formatted table to terminal

--- LuizPadredi_Assignment2.cpp
+++ LuizPadredi_Assignment2.cpp
@@ -3,15 +3,28 @@ Author: Luiz Padredi
 Date: 02/11/2023
 Assignment 2: Loan Calculator
 Repository: https://github.com/Padredilg/loan-calculator
+
+source organizing table
+https://stackoverflow.com/questions/14765155/how-can-i-easily-format-my-data-table-in-c
 */
 
 #include <iostream>
 #include <cmath>
+#include <sstream>
 
 using namespace std;
 
 void getUserInput(double& P, double& r, int& n);
 double calculateAmortization(double P, double r, int n);
+void outputTable(double P, double r, int n, double M);
+
+string columnLable(const string s);
+string rowDoubleValue(const double x);
+string rowIntValue(const int x);
+string printYearEnd(string s, int month);
+
+const int tableWidth = 100;
+const int cellWidth = 17;
 /*
 
 Functions that are needed to run the program:
@@ -19,6 +32,8 @@ Functions that are needed to run the program:
 1. (DONE)- An input function which prompts and fills the input with values for P, r and n.
 
 2. (DONE)- A function that calculates and returns amortization M
+
+3. (IN PROCESS) An output function which displays the table to the screen and writes it to a file.
 
 - A function that calculates the interest and balance for each month.
 Why?
@@ -30,7 +45,7 @@ Why?
     This can be a call-by-reference function.
 
 
-5. - An output function which displays the table to the screen and writes it to a file.
+
 
 */
 
@@ -78,6 +93,8 @@ int main()
     getUserInput(P, r, n);
 
     double M = calculateAmortization(P, r, n);
+
+    outputTable(P, r, n, M);
 
     cout << P << " " << r << " " << n << " " << M;
 
@@ -142,12 +159,87 @@ void getUserInput(double& P, double& r, int& n){
     }
 }
 
+//Calculates the Amortization value
 double calculateAmortization(double P, double r, int n){
-
     double w = pow( (1+r) , n );
-
     double M = P * r * w / ( w - 1 );
-
     return M;
+}
 
+//displays info in table format
+void outputTable(double P, double r, int n, double M){
+    cout << columnLable(" ") << " | "
+         << columnLable("Beginning Balance") << " | "
+         << columnLable("Interest") << " | "
+         << columnLable("Principal") << " | "
+         << columnLable("Ending Balance") << endl
+         << string(tableWidth, '-') << endl;
+
+    double beginningBalance = P;
+    double interest = 10;
+    double principal = 10;
+    double endingBalance = 0;
+
+
+    for(int i=1; i <= n; i++){
+        cout << rowIntValue(i) << " | "
+             << rowDoubleValue(beginningBalance) << " | "
+             << rowDoubleValue(interest) << " | "
+             << rowDoubleValue(principal) << " | "
+             << rowDoubleValue(endingBalance) << "\n";
+
+        if(i%12 == 0){
+            cout << string(tableWidth, '-') << endl
+                 << printYearEnd("END YEAR", i/12)  <<endl
+                 << string(tableWidth, '-') << endl;
+        }
+
+    }
+}
+
+
+//Converts double to string and fits in row space of specified width
+string rowDoubleValue(const double x) {
+    const int width = 17;
+
+    stringstream ss;
+    ss << fixed << right;
+    ss.fill(' ');        // fill space around displayed #
+    ss.width(cellWidth);     // set  width around displayed #
+    ss.precision(2); // set # places after decimal
+    ss << x;
+    return ss.str();
+}
+
+//Converts int to string and fits in row space of specified width
+string rowIntValue(const int x) {
+    stringstream ss;
+    ss << fixed << right;
+    ss.fill(' ');        // fill space around displayed #
+    ss.width(cellWidth);     // set  width around displayed #
+    ss << x;
+    return ss.str();
+}
+
+//Center-aligns string within a field of width w.
+string columnLable(string s) {
+    stringstream ss, spaces;
+    int padding = cellWidth - s.size();                 // count excess room to pad
+    for(int i=0; i<padding/2; ++i)
+        spaces << " ";
+    ss << spaces.str() << s << spaces.str();    // format with padding
+    if(padding>0 && padding%2!=0)               // if odd #, add 1 space
+        ss << " ";
+    return ss.str();
+}
+
+string printYearEnd(string s, int month) {
+    stringstream ss, spaces;
+    int padding = tableWidth - s.size();                 // count excess room to pad
+    for(int i=0; i<padding/2; ++i)
+        spaces << " ";
+    ss << spaces.str() << s << " " << month << spaces.str();    // format with padding
+    if(padding>0 && padding%2!=0)               // if odd #, add 1 space
+        ss << " ";
+    return ss.str();
 }
